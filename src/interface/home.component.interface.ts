d1a08d335d345cc69f2d656e495b5df7042fbff0
HuziG <1067408814@qq.com>
2022-02-22 17:07:43 +0800

    配置组件内容与规范

--- src/interface/home.component.interface.ts
+++ src/interface/home.component.interface.ts
@@ -1,15 +1,17 @@
-export interface HomeComponent {
+export interface HomeComponentParams {
   width: number;
   height: number;
   x: number;
   y: number;
+  yiweiUrl?: string;
 }
 
 export interface HomeDrawerComponent {
   label: string;
+  description?: string;
   name: string;
   style?: {
     color: string;
   };
-  params: HomeComponent;
+  params: HomeComponentParams;
 }
--- src/views/home/static/componentsList.ts
+++ src/views/home/static/componentsList.ts
@@ -1,23 +1,51 @@
 export const components = [
   {
-    label: '新浪新闻',
+    label: '知乎热榜',
+    name: 'YiWeiReBang',
+    params: {
+      width: 500,
+      height: 300,
+      x: 50,
+      y: 50,
+      yiweiUrl: 'https://ionews.top/hot.php?key=ZxUfvKLKuDdSJAl1TKP2EWtlIBzG8T&id=100015&css=card&select='
+    },
+    style: {color: '#0366FE'},
+  },
+  {
+    label: '新浪实时新闻',
     name: 'HotNews',
-    params: { width: 500, height: 300, x: 50, y: 50 },
-    style: { color: '' },
+    params: {width: 500, height: 300, x: 50, y: 50},
+    style: {color: '#DE1D26'},
   },
   {
     label: '网易云评论',
     name: 'MeiZi',
-    params: { width: 500, height: 300, x: 50, y: 50 },
+    params: {width: 500, height: 300, x: 50, y: 50},
+    style: {color: '#D70016'},
   },
   {
     label: '妹子图',
     name: 'MeiZi',
-    params: { width: 500, height: 500, x: 50, y: 50 },
+    description: '漂亮5张妹子图',
+    params: {width: 500, height: 500, x: 50, y: 50},
+    style: {color: '#4F46E5'},
   },
   {
     label: '猫眼电影榜单',
     name: 'MaoYan',
-    params: { width: 300, height: 400, x: 50, y: 50 },
+    params: {width: 300, height: 400, x: 50, y: 50},
+    style: {color: '#F2303D'},
+  },
+  {
+    label: 'Bilibili 7天最热番剧',
+    name: 'BiliHotCarton',
+    params: {width: 300, height: 300, x: 50, y: 50},
+    style: {color: '#FB7199'},
+  },
+  {
+    label: 'Bilibili 最新番剧',
+    name: 'BiliNewestCarton',
+    params: {width: 300, height: 300, x: 50, y: 50},
+    style: {color: '#FB7199'},
   },
 ];
