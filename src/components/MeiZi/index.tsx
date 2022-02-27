596543fadc9e4a999c0cdbf154f4af627b2a1556
HuziG <1067408814@qq.com>
2022-02-27 13:43:27 +0800

    add complete meizi component

--- src/components/MeiZi/index.tsx
+++ src/components/MeiZi/index.tsx
@@ -3,12 +3,14 @@ import './swiper.css';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Mousewheel, Manipulation, Lazy} from 'swiper';
 import 'swiper/css/lazy';
-import LoadingMask from '../Loading';
-import {getMeiZi} from '../../api/meizi';
-import {HomeDrawerComponent} from "../../interface/home.component.interface";
+// import LoadingMask from '../Loading';
+import useResizeObserver from "use-resize-observer";
+import {INFO_CARD_STYLE} from "../../style";
+import {getMeiZi} from "../../api/meizi";
 
-function Index(props: HomeDrawerComponent) {
-  const [loading, setLoading] = useState(true);
+function Index() {
+  // const [loading, setLoading] = useState(true);
+  const {ref, height: scrollHeight} = useResizeObserver<HTMLDivElement>();
   const [peopleData, setPeopleData] = useState<any[] | null>(null);
 
   useEffect(() => {
@@ -27,48 +29,78 @@ function Index(props: HomeDrawerComponent) {
   const handleGetNvLangPicture = async () => {
     const data = await getMeiZi();
     setPeopleData([...data.data]);
-    setLoading(false);
+    // setLoading(false);
   };
 
-  return (
-    <div id={`ALLINFO_${props.name}`} className="mt-10 lg:mt-0 relative rounded-md bg-black overflow-hidden ml-0">
-      {loading && <LoadingMask getData={handleInitData}/>}
+  useEffect(() => {
+    console.log('scrollHeight', scrollHeight)
+  }, [scrollHeight])
 
-      <div className="relative bg-white">
-        <div className="border-8 border-indigo-600">
-          {/* 图片 swiper */}
-          <div
-            id="NvLangImgSwiper"
-            className="flex flex-col align-center justify-center h-60vh lg:h-50vh bg-black"
-          >
-            <Swiper
-              direction={'vertical'}
-              slidesPerView={1}
-              mousewheel
-              lazy
-              loop
-              pagination={{
-                clickable: true,
-              }}
-              navigation
-              modules={[Lazy, Mousewheel, Manipulation]}
-              className="mySwiper"
-            >
-              {peopleData !== null &&
-                peopleData.map((item) => (
-                  <SwiperSlide>
-                    <img
-                      src={item.imgurl}
-                      className="swiper-lazy"
-                      data-example="https://swiperjs.com/demos/images/nature-1.jpg"
-                      alt={'error'}
-                    />
-                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"/>
-                  </SwiperSlide>
-                ))}
-            </Swiper>
-          </div>
-        </div>
+  return (
+    <div
+      ref={ref}
+      className={`bg-black border-8 border-indigo-600 ${INFO_CARD_STYLE}`}
+    >
+      <div
+        className="flex flex-col align-center justify-center bg-black"
+        style={{
+          height: scrollHeight
+        }}
+      >
+        <Swiper
+          direction={'vertical'}
+          slidesPerView={1}
+          mousewheel
+          lazy
+          loop
+          pagination={{
+            clickable: true,
+          }}
+          navigation
+          modules={[Lazy, Mousewheel, Manipulation]}
+        >
+          {peopleData !== null &&
+            peopleData.map((item) => (
+              <SwiperSlide>
+                <img
+                  src={item.imgurl}
+                  className="swiper-lazy"
+                  style={{
+                    height: '100%'
+                  }}
+                  data-example="https://swiperjs.com/demos/images/nature-1.jpg"
+                  alt={'error'}
+                />
+                <div className="swiper-lazy-preloader swiper-lazy-preloader-white"/>
+              </SwiperSlide>
+            ))}
+          {/* { */}
+          {/*  [{ */}
+          {/*    imgurl: 'http://img5.adesk.com/61f4d29ae7bce777a19326a8?sign=06c4eab24980bfe6eaf7609934cbc0a7&t=621b19af', */}
+          {/*  }, */}
+          {/*    { */}
+          {/*      imgurl: 'http://img5.adesk.com/620093e3e7bce777b68489d5?sign=25a619c97bcc80986296a32889d5079c&t=621b19af', */}
+          {/*    }, */}
+          {/*    { */}
+          {/*      imgurl: 'http://img5.adesk.com/615524b4e7bce730af642cc2?sign=465730e28a95cc5e5be15abb7a83d34b&t=621b19af', */}
+          {/*    }, */}
+          {/*    { */}
+          {/*      imgurl: 'http://img5.adesk.com/615524b4e7bce7306e879ef1?sign=df450ac8640c718f5c777947405cf816&t=621b19af', */}
+          {/*    }].map((item) => ( */}
+          {/*    <SwiperSlide> */}
+          {/*      <img */}
+          {/*        src={item.imgurl} */}
+          {/*        className="swiper-lazy" */}
+          {/*        style={{ */}
+          {/*          height: '100%', */}
+          {/*        }} */}
+          {/*        alt={'error'} */}
+          {/*      /> */}
+          {/*      <div className="swiper-lazy-preloader swiper-lazy-preloader-white"/> */}
+          {/*    </SwiperSlide> */}
+          {/*  )) */}
+          {/* } */}
+        </Swiper>
       </div>
     </div>
   );
