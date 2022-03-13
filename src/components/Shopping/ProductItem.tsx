740630d3be2996a3feb067a3bd44cd3dfdfbed4f
HuziG <1067408814@qq.com>
2022-03-13 19:05:37 +0800

    add tb module

---
+++ src/components/Shopping/ProductItem.tsx
@@ -0,0 +1,39 @@
+import { TaoQiangGouItem } from '../../interface/taoqianggou.inerface';
+
+function ProductItem({ item }: { item: TaoQiangGouItem }) {
+  return <div className={'p-1 h-full overflow-auto'}>
+    <img className={'rounded-xl h-56 bg-gray-400 overflow-hidden'} src={item.pict_url} alt='error' />
+    <div className={'py-1'}>
+      <span 
+        className={`text-white text-xs rounded-sm px-1 mr-1`}
+        style={{
+          backgroundColor: item.user_type === '1' ? '#FD0237' : '#FF5601'
+        }}
+      >
+        {
+          item.user_type === '1' ? '天猫' : '淘宝'
+        }
+      </span>
+      <span className={'product-title text-sm'}>{item.title}</span>
+      {
+        item.item_description && 
+        <div className={'product-desc text-xs text-gray-500 py-2'}>{item.item_description}</div>
+      }
+      
+      <div>
+        <div className={'inline-block'} style={{
+          color: '#FF5601'
+        }}>
+          <span className={'text-xs'}>￥</span>
+          <span className={'font-bold text-lg mr-1'}>{item.quanhoujia}</span>
+          <span className={'text-xs mr-1'}>券后价</span>
+        </div>
+        <span className={'text-xs text-gray-500'}>
+          {item.volume}人付款
+        </span>
+      </div>
+    </div>
+  </div>;
+}
+
+export default ProductItem;
---
+++ src/interface/taoqianggou.inerface.ts
@@ -0,0 +1,14 @@
+export interface TaoQiangGouItem {
+  item_id: number;
+  small_images: string[];
+  user_type: string; // 	0为淘宝店铺，1为天猫店铺
+  shop_title: string; //  店铺名
+  title: string;
+  num_iid: string;
+  pict_url: string;
+  volume: string; // 销量
+  item_description: string;
+  zk_final_price: string; // 在售价
+  quanhoujia: string; // 券后价
+  url: string; // 跳转链接
+}
