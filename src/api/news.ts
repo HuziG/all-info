8f2d2bf28b05dc20ae75e73f01af65d7b8e1eb09
HuziG <1067408814@qq.com>
2022-05-09 14:26:02 +0800

    git reset

--- src/api/news.ts
+++
@@ -1,7 +0,0 @@
-import request from '../utils/request';
-
-/**
- * 获取热点新闻
- * @returns {Promise<AxiosResponse<any>|{err, type: string}>}
- */
-export const getNews = () => request('/news');
--- src/api/taonvlang.ts
+++
@@ -1,8 +0,0 @@
-import request from '../utils/request';
-
-/**
- * 获取热点新闻
- * @returns {Promise<AxiosResponse<any>|{err, type: string}>}
- */
-export const getNvLangPicture = (type: string, page: number) =>
-  request(`/nvlang?type=${type}&order=1&page=${page}`);
